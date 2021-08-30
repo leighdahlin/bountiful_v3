cfb81654d15f0f1cb7053b5374921355be8d1761
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-08-30 11:16:37 -0700

    added media queries to user dashboard and seller profile for mobile responsiveness

--- client/src/pages/MyBounty.js
+++ client/src/pages/MyBounty.js
@@ -113,7 +113,7 @@ export default function MyBounty() {
     return(
     <div id="dashboard-image" className="dashboard-container">
         <div className="d-flex">
-            <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <div className="nav nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                 <button className="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Listings</button>
                 <button className="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
                 <button className="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
