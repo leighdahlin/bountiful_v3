b1204a532946665080a143abf4990f308ec25d17
truethatplaya <alina.hernandez.ca@gmail.com>
2021-09-07 15:53:57 -0700

    added values & edited the variables.

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -5,6 +5,8 @@ export default function Review() {
   const [reviewContent, setContent] = useState({
     rating: 5,
     reviewee: "", //need a user ID
+    title: "",
+    review: "",
   });
 
   const reviewChange = (event) => {
@@ -15,57 +17,61 @@ export default function Review() {
     });
   };
 
-  // const reviewFormSubmit = async (event) => {
-  //   event.preventDefault();
-  //   try {
-  //     const mutationResponse = await login({
-  //       //TODO: what function or mutation should I call here?
-  //       variables: {
-  //         reviewTitle: reviewContent.title,
-  //         reviewContent: reviewContent.review,
-  //         starRating: reviewContent.rating,
-  //         reviewee: reviewContent.reviewee,
-  //       },
-  //     });
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
+  const reviewFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        //TODO: what function or mutation should I call here?
+        variables: {
+          // reviewTitle: reviewContent.title,
+          body: reviewContent.review,
+          // username:
+          createdAt: new Date().toLocaleString(),
+          // starRating: reviewContent.rating,
+          // reviewee: reviewContent.reviewee,
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
-      <div className="review-container">
-        <form className="animate login-form">
-          <div className="container">
-            <label htmlFor="title">
-              <b>Title</b>
-            </label>
-            <input
-              id="title"
-              type="text"
-              placeholder="Title"
-              name="title"
-              onChange={reviewChange}
-              required
-            />
-            <label htmlFor="review-content">
-              <b>Review</b>
-            </label>
-            <input
-              id="review"
-              type="text"
-              placeholder="Enter Review"
-              name="review"
-              onChange={reviewChange}
-              required
-            />
-          </div>
-          <div className="container" style={{ background: "var(--gray)" }}>
-            <button type="submit" className="submitbtn">
-              Submit Review
-            </button>
-          </div>
-        </form>
-      </div>
+    <div className="review-container">
+      <form className="animate login-form" onSubmit={reviewFormSubmit}>
+        <div className="container">
+          <label htmlFor="title">
+            <b>Title</b>
+          </label>
+          <input
+            id="title"
+            type="text"
+            placeholder="Title"
+            name="title"
+            value={reviewContent.title}
+            onChange={reviewChange}
+            required
+          />
+          <label htmlFor="review-content">
+            <b>Review</b>
+          </label>
+          <input
+            id="review"
+            type="text"
+            placeholder="Enter Review"
+            name="review"
+            value={reviewContent.review}
+            onChange={reviewChange}
+            required
+          />
+        </div>
+        //TODO add create at //TODO add seller
+        <div className="container" style={{ background: "var(--gray)" }}>
+          <button type="submit" className="submitbtn">
+            Submit Review
+          </button>
+        </div>
+      </form>
+    </div>
   );
-};
-
+}
