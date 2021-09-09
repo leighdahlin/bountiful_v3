d9ee2d109db4db7c6eb53ed99e24f6a8b208083d
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-09-08 18:51:00 -0700

    working on user profile update form

--- client/src/components/ProfileInfo.js
+++ client/src/components/ProfileInfo.js
@@ -146,9 +146,10 @@ export default function ProfileInfo({ profile }) {
             <div className="profile">
                 <img id="profile-img" src={profileImage} alt="profile"/>
                 <div className="profile-info">
-                    <h6 id="dashboard-firstname">{profile.firstname}</h6>
+                    <h6 id="dashboard-firstname">Name: {profile.first_name} {profile.last_name}</h6>
                     <h6 id="dashboard-username">{profile.username}</h6>
                     <h6 id="dashboard-location">{profile.location}</h6>
+                    <h6 id="dashboard-email">{profile.email}</h6>
             
                     <button id="edit-profile" type="button" className="btn" data-id={profile._id} onClick={editProfile}>Edit</button>
                 
