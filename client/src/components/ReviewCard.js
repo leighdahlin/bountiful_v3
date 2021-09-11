19cbf7859eadf948da1c084c8e44e4a93d992152
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-09-11 09:45:41 -0700

    updated review component to pull in review query and pass it in as props to review card to display reviews

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -1,13 +1,15 @@
+import formatTime from "../utils/helpers.js";
+
 export default function ReviewCard({ reviewData }) {
         if(!reviewData[0]){
             return(
-                <div>You don't have any listings yet!</div>
+                <div>Be the first to review!</div>
             )
         }
     
         if(reviewData[0]){
-            return items.map((review) => (
-                <div id = "item-card" key={review._id} className="card mb-3 item-card" style={{maxWidth: "500px"}}>
+            return reviewData.map((review) => (
+            <div id = "item-card" key={review._id} className="card mb-3 item-card" style={{maxWidth: "500px"}}>
                 <div className="row g-0">
                     {/* <div className="col-md-4 card-pic">
                         <div className="item-buttons">
@@ -19,23 +21,15 @@ export default function ReviewCard({ reviewData }) {
                                 <h2 className="item-title"><strong>{review.title}</strong> </h2>
                             </div>
                             <div className="card-body">
-                                <p className="card-text">{review.user.username}</span></p>
-                                <p className="card-text">{review.body}</span></p>
-        
-                                <p className="card-text">Price: $ <span className="item-price">{item.item_price}</span> / <span className="item-unit">{item.item_unit}</span></p>
-                                <p className="card-text">Quantity: <span className="item-quantity">{item.item_quantity}</span></p>
-                                
-                                <p className="card-text">Description: <span className="item-description">{item.item_description}</span></p>
-                                <p className="card-text"><small className="text-muted">Date created: {formatTime(item.createdAt)}</small></p>
-                                <div className="center-btns">
-                                    <button id = {item.id} type="button" className="btn item-btn edit-item" onClick={toggleItem}>Edit</button>   
-                                    <button type="button" className="btn item-btn delete-item" data-id={item._id} onClick={handleDelete}>Delete</button>           
-                                </div>
+                                <p className="card-text">{review.user.username}</p>
+                                <p className="card-text">{review.rating}</p>
+                                <p className="card-text">{review.body}</p>
+                                <p className="card-text"><small className="text-muted">Date created: {formatTime(review.createdAt)}</small></p>
                             </div>
                     </div>
                 </div>
             </div>
-            ));
+        ));
         
         }
 }
