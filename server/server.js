4c28348988c1e176ffd7f0a68c966cfb7f19d425
aanolan51 <a.a.nolan@hotmail.com>
2021-09-11 15:02:32 -0700

    heroku deployment

--- server/server.js
+++ server/server.js
@@ -24,8 +24,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// app.get('*', (req,res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 db.once('open', () => {
