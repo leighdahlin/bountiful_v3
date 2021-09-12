48aefc71958e7cdc15dcfec0a0361e8cda84443e
truethatplaya <alina.hernandez.ca@gmail.com>
2021-09-11 17:25:55 -0700

    changed board radius on submit review button. made a review card.css file. imported reviewCard.css to ReviewCard.js file.

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -1,35 +1,44 @@
 import formatTime from "../utils/helpers.js";
+import "../assets/css/reviewCard.css";
 
 export default function ReviewCard({ reviewData }) {
-        if(!reviewData[0]){
-            return(
-                <div>Be the first to review!</div>
-            )
-        }
-    
-        if(reviewData[0]){
-            return reviewData.map((review) => (
-            <div id = "item-card" key={review._id} className="card mb-3 item-card" style={{maxWidth: "500px"}}>
-                <div className="row g-0">
-                    {/* <div className="col-md-4 card-pic">
+  if (!reviewData[0]) {
+    return <div>Be the first to review!</div>;
+  }
+
+  if (reviewData[0]) {
+    return reviewData.map((review) => (
+      <div
+        id="item-card"
+        key={review._id}
+        className="card mb-3 item-card"
+        style={{ maxWidth: "500px" }}
+      >
+        <div className="row g-0">
+          {/* <div className="col-md-4 card-pic">
                         <div className="item-buttons">
                         </div>
                         <img src={logo} className="img-fluid rounded-start items-b-logo" alt={item.item_name}/>
                     </div> */}
-                    <div id="card-id" className="col-md-8 card-id" data-id={review._id}>
-                            <div className="card-title">
-                                <h2 className="item-title"><strong>{review.title}</strong> </h2>
-                            </div>
-                            <div className="card-body">
-                                <p className="card-text">{review.user.username}</p>
-                                <p className="card-text">{review.rating}</p>
-                                <p className="card-text">{review.body}</p>
-                                <p className="card-text"><small className="text-muted">Date created: {formatTime(review.createdAt)}</small></p>
-                            </div>
-                    </div>
-                </div>
+          <div id="card-id" className="col-md-8 card-id" data-id={review._id}>
+            <div className="card-title">
+              <h2 className="item-title">
+                <strong>{review.title}</strong>{" "}
+              </h2>
             </div>
-        ));
-        
-        }
+            <div className="card-body">
+              <p className="card-text">{review.user.username}</p>
+              <p className="card-text">{review.rating}</p>
+              <p className="card-text">{review.body}</p>
+              <p className="card-text">
+                <small className="text-muted">
+                  Date created: {formatTime(review.createdAt)}
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    ));
+  }
 }
